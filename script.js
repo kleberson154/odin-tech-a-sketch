dfd2480a918906ee374c2d0fd33424f5b08b8c4e
kleberson154 <kleberson55@hotmail.com>
2023-12-21 15:54:54 -0300

    Adicionado a funcao removeGridCells

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ const GRIDSIDE = 600
 let squaresPerSide = 16
 
 const sketchArea = document.querySelector('#sketch-area')
+const sliderContainer = document.querySelector('#slider-container')
+const slider = document.querySelector('#slider')
+const sliderValue = document.querySelector('#slider-value')
+
+sliderValue.textContent = `${slider.value} x ${slider.value} (Resolution)`
 sketchArea.style.width = sketchArea.style.height = `${GRIDSIDE}px`
 
 function changeBackgroundColor() {
   this.style.backgroundColor = 'black'
 }
 
-function createGridCells() {
+function createGridCells(squaresPerSide) {
   const numOfSquares = squaresPerSide * squaresPerSide
   const widthOrHeight = `${GRIDSIDE / squaresPerSide - 2}px`
   for (let i = 0; i < numOfSquares; i++) {
@@ -23,4 +28,10 @@ function createGridCells() {
   }
 }
 
-createGridCells()
+function removeGridCells() {
+  while (sketchArea.firstChild) {
+    sketchArea.removeChild(sketchArea.firstChild)
+  }
+}
+
+createGridCells(16)
